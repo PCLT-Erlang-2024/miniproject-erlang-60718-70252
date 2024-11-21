87c0e4d86d9d86019fb1d5e6411367fcc98e7910
Diogo-A-Envia-Matos <94308407+Diogo-A-Envia-Matos@users.noreply.github.com>
2024-11-21 15:38:42 +0000

    Task 2 done.

---
+++ task2.erl
@@ -0,0 +1,45 @@
+-module(task2).
+-export([start/0, start/3, producer/3, conveyor/1, truck/1, truck/2]).
+
+start() -> start(10, 10, 10).
+start(NLines, NProductions, TruckSize) -> [spawn(?MODULE, producer, [spawn(?MODULE, conveyor, [spawn(?MODULE, truck, [TruckSize])]), NProductions, TruckSize]) || _ <- lists:seq(1,NLines)].
+
+producer(ConvId, 0, _) ->
+	ConvId ! stop,
+	io:format("Producer ~p stopped~n", [self()]);
+producer(ConvId, NProductions, MaxSize) ->
+	Size = rand:uniform(MaxSize),
+	io:format("Producer ~p sending package (~p)~n", [self(), Size]),
+	ConvId ! {package, NProductions, Size},
+	producer(ConvId, NProductions - 1, MaxSize).
+
+conveyor(TruckId) ->
+	receive
+		{package, PackNum, Size} ->
+			io:format("Conveyor ~p received package (~p)~n", [self(), size]),
+			TruckId ! {package, PackNum, Size},
+			conveyor(TruckId);
+		stop ->
+			io:format("Conveyor ~p stopped~n", [self()]),
+			TruckId ! stop
+	end.
+
+truck(TruckSize) -> truck(TruckSize, TruckSize).
+truck(TruckSize, CurrentSize) ->
+	receive
+		{package, PackNum, Size} ->
+			if 
+				size >= TruckSize -> 
+					io:format("Truck ~p departed because package ~p (~p/~p) was too big~n", [self(), PackNum, Size, CurrentSize]),
+					truck(TruckSize, TruckSize, {package, PackNum, Size});
+				size < TruckSize -> 
+					io:format("Truck ~p received package (~p/~p)~n", [self(), size, CurrentSize]),
+					truck(TruckSize, CurrentSize - size)
+			end;
+		stop ->
+			io:format("Truck ~p stopped~n", [self()])
+	end.
+
+truck(TruckSize, CurrentSize, {package, PackNum, Size}) ->
+	io:format("New Truck ~p has arrived and picked up package ~p (~p/~p)~n", [self(), PackNum, Size, CurrentSize]),
+	truck(TruckSize, CurrentSize - Size).
